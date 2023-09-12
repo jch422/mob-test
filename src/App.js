5e7dc89e7430937e18ea928af94fe30188dc6f2c
Jichan Chung <jc.chung@3billion.io>
2023-09-12 10:47:42 +0900

    feat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
 function App() {
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
-    <div style={{ background: "salmon", height: "100vh" }}>
-      <div>hi</div>
+    <div
+      {...getRootProps({
+        style: {
+          border: "2px solid green",
+        },
+      })}
+    >
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
     </div>
   );
 }
